86c3caad8ed5cedabde93f8bee1f48f88f79e6c0
Marco Apraez <maz_2002@email.com>
2020-02-08 10:24:31 -0800

    apraez_view_tool initial commit

---
+++ lib/apraez_view_tool.rb
@@ -0,0 +1,7 @@
+require "apraez_view_tool/version"
+require "apraez_view_tool/renderer"
+
+module ApraezViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/apraez_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ApraezViewTool
+  VERSION = "0.1.0"
+end
